4e40c87ca0113029a8fa477cfa3db509d6927704
PeniPeniYama <sinngo_band@yahoo.co.jp>
2016-12-24 21:57:37 +0900

    [add] shop, user seedファイル

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(
+:id => 1,
+:name => "ユーザ１"
+)
+User.create(
+:id => 2,
+:name => "ユーザ２"
+)
+User.create(
+:id => 3,
+:name => "ユーザ３"
+)
+User.create(
+:id => 4,
+:name => "ユーザ４"
+)
+User.create(
+:id => 5,
+:name => "ユーザ５"
+)
+
+Shop.create(
+:id => 1,
+:name => "店舗１",
+:image => Rails.root.join("db/fixtures/shop_1.jpg").open
+)
