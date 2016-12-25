2446c75b964b9fe14a36923ddb95b2c722fc8ed7
PeniPeniYama <sinngo_band@yahoo.co.jp>
2016-12-25 22:02:36 +0900

    [add] ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def current_user
+    if session[:user_id].blank?
+      session[:user_id] = 1
+    elsif params[:q].present? and (session[:user_id] != params[:q])
+      session[:user_id] = params[:q]
+    end
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+
+  helper_method :current_user
+
 end
