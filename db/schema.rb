710d6e24ca9637a50387b8878c30410ec36e75bc
PeniPeniYama <sinngo_band@yahoo.co.jp>
2016-12-30 15:27:36 +0900

    [add] いいねテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227122422) do
+ActiveRecord::Schema.define(version: 20161230062555) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -21,12 +21,20 @@ ActiveRecord::Schema.define(version: 20161227122422) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "shop_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "shops", force: :cascade do |t|
     t.string   "name"
     t.text     "body"
     t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "likes_count"
   end
 
   create_table "users", force: :cascade do |t|
