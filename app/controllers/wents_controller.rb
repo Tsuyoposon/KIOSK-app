a5803effa15561554dce8f54c6e25058d2cff714
msk6252 <msk6252@yahoo.co.jp>
2017-01-08 02:16:59 +0900

    [modify] jsの修正

--- app/controllers/wents_controller.rb
+++ app/controllers/wents_controller.rb
@@ -1,7 +1,7 @@
 class WentsController < ApplicationController
 	def create
     	@went = Went.create(user_id: current_user.id, shop_id: params[:shop_id])
-    	@likes = Went.where(shop_id: params[:shop_id])
+    	@wents = Went.where(shop_id: params[:shop_id])
     	@shops = Shop.all
   	end
 
--- db/fixtures/seeds.rb
+++ db/fixtures/seeds.rb
@@ -24,71 +24,83 @@ Shop.create(
 :id => 1,
 :name => "店舗１",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 2,
 :name => "店舗２",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 3,
 :name => "店舗３",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 4,
 :name => "店舗４",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 5,
 :name => "店舗５",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 6,
 :name => "店舗６",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 7,
 :name => "店舗７",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 8,
 :name => "店舗８",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 9,
 :name => "店舗９",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 10,
 :name => "店舗１０",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 11,
 :name => "店舗１１",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
 Shop.create(
 :id => 12,
 :name => "店舗１２",
 :body => "おいしいナポリタンのお店です",
-:image => Rails.root.join("db/fixtures/shop_2.jpg").open
+:image => Rails.root.join("db/fixtures/shop_2.jpg").open,
+:wents_count => 0
 )
