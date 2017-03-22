7b0fa374bd955abb291e8f4a582389a9cf032ae3
msk6252 <msk6252@yahoo.co.jp>
2017-03-22 13:41:01 +0900

    [modify] 行ったよ機能とマップの連携

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -5,5 +5,7 @@ class MapController < ApplicationController
     @likes = Like.all
     @c_likes = CLike.all
     @wents = Went.all
+    @user = User.all
+    @current_user = @user.find_by(id: session[:user_id])
   end
 end
